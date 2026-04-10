c72ddd41e9d0e7eea867adae374bc5104cc2a43f
wangkai000 <1071231288@qq.com>
2026-04-11 00:47:04 +0800

    fix: 友链移动端一行显示一个

--- src/pages/Friend.vue
+++ src/pages/Friend.vue
@@ -720,7 +720,7 @@ watchOnce(giscusRootRef, (root) => {
           </div>
 
           <div
-            class="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 md:gap-4"
+            class="grid grid-cols-1 gap-3 md:grid-cols-3 lg:grid-cols-4 md:gap-4"
           >
             <a
               v-for="(blog, index) in personalBlogs"
